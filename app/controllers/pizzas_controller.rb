282766816f1f362e46bab73304c8278a7dedf075
edna1996 <guantaiedna@gmapl.com>
2023-03-20 23:25:52 +0300

    added crud functions

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,2 +1,16 @@
 class PizzasController < ApplicationController
+    def index
+        pizzas = Pizza.all
+        render json: pizzas, include: :restraunts
+      end
+      def show
+          pizza = find_pizzas
+          render json: pizza, include: :restraunts
+      end
+  
+  private
+  def find_pizzas
+      pizza = Pizza.find(params[:id])
+  end
+      
 end
