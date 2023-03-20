5a7042f6aa9011c50aaef891a5bfa0f4e7787044
edna1996 <guantaiedna@gmapl.com>
2023-03-20 22:47:42 +0300

    created data for restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+#10 restaurants
+puts "started seeeeedding!!"
+
+10.times do
+  name = Faker::Restaurant.name
+  address = Faker::Address.full_address
+  Restaurant.create(name: name, address: address)
+end
