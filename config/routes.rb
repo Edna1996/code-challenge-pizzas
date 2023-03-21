c6045b6e5d03650c3082ce73063ba2dd33bb07ee
edna1996 <guantaiedna@gmapl.com>
2023-03-21 15:54:11 +0300

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   resources :restaurant_pizzas, only: [:index,:create]
-  resources :pizzas, only: [:index]
+  resources :pizzas, only: [:index, :show]
   resources :restaurants, only: [:index,:show,:destroy]
   
 end
