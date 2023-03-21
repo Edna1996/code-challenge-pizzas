b08a4dad99675696027b8b5a7269766556c90aa5
edna1996 <guantaiedna@gmapl.com>
2023-03-21 10:20:57 +0300

    completed code challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 
-# Create 10 restaurants
+# Create 20 restaurants
 puts "started seeeeedding!!"
 
-10.times do
+20.times do
   name = Faker::Restaurant.name
   address = Faker::Address.full_address
   Restaurant.create(name: name, address: address)
@@ -15,13 +15,13 @@ end
   Pizza.create(name: name, ingredients: ingredients)
 end
 
-# Create 30 restaurant_pizzas
-30.times do
+# Create 20 restaurant_pizzas
+20.times do
   name = Faker::Food.dish
   #ingredients = Faker::Food.ingredient
-  price = rand(40..200)
-  pizza_id = rand(1..20)
-  restaurant_id = rand(1..10)
+  price = rand(1...30)
+  pizza_id = rand(1...20)
+  restaurant_id = rand(1...20)
   RestaurantPizza.create(name: name, price: price, pizza_id: pizza_id, restaurant_id: restaurant_id)
 end
 
